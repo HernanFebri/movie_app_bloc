69e6752050ee6d30135d604b028ba9ff706b7616
Hernan Febri <hernanfebri@gmail.com>
2024-11-13 12:10:34 +0700

    Build app with bloc pattern

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xffE21221);
+  static const background = Color(0xff191A20);
+  static const secondBackground = Color(0xff35383F);
+}
